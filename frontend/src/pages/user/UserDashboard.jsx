df1ab1c9c3afacfd6b77243e53e4ca2d37f79fc0
Vikram Panchal <120727321+Vikram-panchal@users.noreply.github.com>
2025-05-31 13:39:25 +0530

    greeting fix

--- frontend/src/pages/user/UserDashboard.jsx
+++ frontend/src/pages/user/UserDashboard.jsx
@@ -63,17 +63,28 @@ const UserDashboard = () => {
     navigate("/admin/tasks");
   };
 
+  let greeting = "";
   useEffect(() => {
     getDashboardData();
+    const hour = new Date().getHours();
+    greeting =
+      hour < 12
+        ? "Good Morning"
+        : hour < 18
+        ? "Good Afternoon"
+        : "Good Evening";
+
     return () => {};
   }, []);
-  console.log(dashboardData);
+
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className="card my-5">
         <div>
           <div className="col-span-3">
-            <h2 className="text-xl md:text-2xl">Good Morning! {user?.name}</h2>
+            <h2 className="text-xl md:text-2xl">
+              {greeting} {user?.name || "Guest"}
+            </h2>
             <p className="text-xs md:text-[13px] text-gray-400 mt-1.5">
               {moment().format("dddd Do MMM YYYY")}
             </p>
